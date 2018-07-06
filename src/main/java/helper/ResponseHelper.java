c274bbfd9dc1742d2dd008e743023cd096e839bc
a.gordynets <a.gordynets@godeltech.com>
2018-07-06 15:44:00 +0300

    -commit add git api test

---
+++ src/main/java/helper/ResponseHelper.java
@@ -0,0 +1,6 @@
+package helper;
+
+public class ResponseHelper {
+    final static String baseUrl = "http://www.google.com";
+
+}
--- src/test/java/cucumber/steps/Steps.java
+++ src/test/java/cucumber/steps/Steps.java
@@ -3,9 +3,15 @@ package cucumber.steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Steps{
 
@@ -27,7 +33,21 @@ public class Steps{
 
     @Then("^I should see \"([^\"]*)\" status code$")
     public void iShouldSeeStatusCode(String arg0) {
-        int code = given().when().get("http://www.google.com").statusCode();//then().statusCode(200);
-        Assert.assertEquals(code, Integer.parseInt(arg0));
+        RestAssured.baseURI = "https://api.github.com/";
+        RequestSpecification httpReq = RestAssured.given();
+        Response response = httpReq.get("users/batuwka");
+
+        ResponseBody body = response.getBody();
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println(body.prettyPrint());
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+//        InputStream xsd = Thread.currentThread().getContextClassLoader().getResourceAsStream("schemas/mainPage.xsd");
+//        when().get(RestAssured.baseURI + "/complete").then().statusCode(Integer.parseInt(arg0)).body(matchesXsd(xsd));
+        get("users/batuwka").then().body("public_gists", equalTo(1));
     }
 }
